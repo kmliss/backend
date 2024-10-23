d80297eee31956b013255465c6d221b0ca8462d0
kmliss <156055887+kmliss@users.noreply.github.com>
2024-10-23 22:26:45 +1000

    Update settings.py

--- back/back/settings.py
+++ back/back/settings.py
@@ -24,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+BACKEND = config('BACKEND_IP')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', BACKEND]
 
 
 # Application definition
